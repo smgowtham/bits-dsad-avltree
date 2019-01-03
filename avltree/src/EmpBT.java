ba042328978428b88a7c19ff49502d36665e88d8
Gowtham <maheswg@amazon.com>
2019-01-03 19:43:21 +0530

    Adding till operation 5, has bugs

--- avltree/src/EmpBT.java
+++ avltree/src/EmpBT.java
@@ -1,6 +1,142 @@
 public class EmpBT {
     EmployeeNode root;
 
+    /***
+     * Operation 1 of the assignment
+     *
+     * Inserts the employee node into the tree
+     *
+     * @param node The input root node
+     * @param empId The employee Id to be inserted
+     * @return Address of the Root of the balanced BST
+     */
+    EmployeeNode readEmployees(EmployeeNode node, int empId) { 
+  
+        /* 1.  Perform the normal BST insertion */
+        if (node == null)
+            return (new EmployeeNode(empId));
+
+        if (empId < node.empId)
+            node.left = readEmployees(node.left, empId);
+        else if (empId > node.empId)
+            node.right = readEmployees(node.right, empId);
+        else // Duplicate empIds not allowed 
+            node.attCount += 1; 
+  
+        /* 2. Update height of this ancestor node */
+        node.height = 1 + max(height(node.left),
+                height(node.right)); 
+  
+        /* 3. Get the balance factor of this ancestor 
+              node to check whether this node became 
+              unbalanced */
+        int balance = getBalance(node);
+
+        // If this node becomes unbalanced, then there 
+        // are 4 cases Left Left Case 
+        if (balance > 1 && empId < node.left.empId)
+            return rightRotate(node);
+
+        // Right Right Case 
+        if (balance < -1 && empId > node.right.empId)
+            return leftRotate(node);
+
+        // Left Right Case 
+        if (balance > 1 && empId > node.left.empId) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+
+        // Right Left Case 
+        if (balance < -1 && empId < node.right.empId) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        } 
+  
+        /* return the (unchanged) node pointer */
+        return node;
+    }
+
+    /**
+     * Operation 2 in assignment.
+     */
+    int getHeadcount(EmployeeNode emp) {
+        if (emp == null)
+            return 0;
+
+        int res = 0;
+        if (emp.left != null && emp.right != null)
+            res++;
+
+        res += (getHeadcount(emp.left) + getHeadcount(emp.right));
+        return res;
+    }
+
+    /***
+     * Operation 3 of the assignment.
+     * 
+     * @param emp The Root node of the employee Tree
+     * @param id The Employee Id to search
+     * @return True, If employee exists
+     */
+    boolean searchID(EmployeeNode emp, int id) {
+        if (emp != null) {
+            if (emp.empId == id)
+                return true;
+            else if (id < emp.empId) // Search in left tree
+              searchID(emp.left, id);
+            else // Search in right tree
+                searchID(emp.left, id);
+        }
+        return false;
+    }
+
+    /***
+     * Operation 4 of the Assignment.
+     * 
+     * Search howoften an employee enters the org
+     * @param emp The Root node of the employee Tree
+     * @param id The Employee Id to search
+     * @return The number of time the employee enters
+     */
+    int howOften(EmployeeNode emp, int id) {
+        if (emp != null) {
+            if (emp.empId == id)
+                return emp.attCount;
+            else if (id < emp.empId) // Search in left tree
+                searchID(emp.left, id);
+            else // Search in right tree
+                searchID(emp.left, id);
+        }
+        return 0;
+    }
+
+    /***
+     * Operation 5 of the Assignment.
+     *
+     * Search frequentVisitor employee of the org
+     * @param emp The Root node of the employee Tree
+     * @return The number of time the employee enters
+     */
+    
+    EmployeeNode frequentVisitor(EmployeeNode emp) {
+        if (emp == null)
+            return null;
+
+        EmployeeNode maxNode = emp;
+
+        EmployeeNode leftMaxNode = frequentVisitor(emp.left);
+        EmployeeNode rightMaxNode = frequentVisitor(emp.right);
+
+        if (leftMaxNode.getAttCount() > maxNode.getAttCount())
+            maxNode = leftMaxNode;
+        if (rightMaxNode.getAttCount() > maxNode.getAttCount())
+            maxNode = rightMaxNode;
+        
+        return maxNode;
+    }
+    
+
     // A utility function to get the height of the tree 
     int height(EmployeeNode N) {
         if (N == null)
@@ -58,52 +194,6 @@ public class EmpBT {
         return height(N.left) - height(N.right);
     }
 
-    EmployeeNode insert(EmployeeNode node, int empId) { 
-  
-        /* 1.  Perform the normal BST insertion */
-        if (node == null)
-            return (new EmployeeNode(empId));
-
-        if (empId < node.empId)
-            node.left = insert(node.left, empId);
-        else if (empId > node.empId)
-            node.right = insert(node.right, empId);
-        else // Duplicate empIds not allowed 
-            node.attCount += 1; 
-  
-        /* 2. Update height of this ancestor node */
-        node.height = 1 + max(height(node.left),
-                height(node.right)); 
-  
-        /* 3. Get the balance factor of this ancestor 
-              node to check whether this node became 
-              unbalanced */
-        int balance = getBalance(node);
-
-        // If this node becomes unbalanced, then there 
-        // are 4 cases Left Left Case 
-        if (balance > 1 && empId < node.left.empId)
-            return rightRotate(node);
-
-        // Right Right Case 
-        if (balance < -1 && empId > node.right.empId)
-            return leftRotate(node);
-
-        // Left Right Case 
-        if (balance > 1 && empId > node.left.empId) {
-            node.left = leftRotate(node.left);
-            return rightRotate(node);
-        }
-
-        // Right Left Case 
-        if (balance < -1 && empId < node.right.empId) {
-            node.right = rightRotate(node.right);
-            return leftRotate(node);
-        } 
-  
-        /* return the (unchanged) node pointer */
-        return node;
-    }
 
     // A utility function to print preorder traversal 
     // of the tree. 
--- avltree/src/EmployeeAttendance.java
+++ avltree/src/EmployeeAttendance.java
@@ -6,23 +6,107 @@ public class EmployeeAttendance {
 
     public static void main(String[] args) throws IOException {
         
-        File file =
-                new File("./avltree/testdata/input1.txt");
+        File file = new File("./avltree/src/input.txt");
         Scanner sc = new Scanner(file);
         
         EmpBT employeeTree = new EmpBT();
 
         while (sc.hasNextLine())
         {
-            int employeeId = Integer.parseInt(sc.nextLine());
-            System.out.println("Inserting Node: " + employeeId);
+            try {
+                int employeeId = Integer.parseInt(sc.nextLine());
+                employeeTree.root = employeeTree.readEmployees(employeeTree.root, employeeId);
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.println("File contains invalid inputs. Use integer values.");
+                System.exit(1);
+            }
+//            System.out.println("Inserting Node: " + employeeId);
             
-            employeeTree.root = employeeTree.insert(employeeTree.root, employeeId);
+           
         }
 
-        System.out.println("Preorder traversal" +
-                " of constructed tree is : ");
-        employeeTree.preOrder(employeeTree.root);
+//        System.out.println("Preorder traversal" + " of constructed tree is : ");
+        loop: while(true) {
+            System.out.println("\nEnter the Operation to do:");
+            System.out.println("-----------------------------");
+            System.out.println("0. Exit");
+            System.out.println("1. Print tree");
+            System.out.println("2. Get HeadCount ");
+            System.out.println("3. searchID");
+            System.out.println("4. howOften");
+            System.out.println("5. frequentVisitor");
+            System.out.println("6. printRangePresent");
+            System.out.println("-----------------------------");
+            System.out.println("Enter your choice:");
+
+            try {
+                int inputChoice = getInput();
+
+                if (inputChoice == -1)
+                    inputChoice = Integer.parseInt(sc.nextLine());
+
+                int searchId = 0;
+                switch (inputChoice) {
+                case 0:
+                    System.out.println("Bye");
+                    break loop;
+                case 1:
+                    System.out.println("Preorder travesal of tree:");
+                    employeeTree.preOrder(employeeTree.root);
+                    break;
+                case 2:
+                    System.out.println("Number of employees in org: " + employeeTree.getHeadcount(employeeTree.root));
+                    break;
+                case 3:
+                    System.out.println("Enter the employee Id to Search:");
+                    searchId = getInput();
+                    if (employeeTree.searchID(employeeTree.root, searchId))
+                        System.out.println("Employee Id " + searchId + "is Present.");
+                    else
+                        System.out.println("Employee Id " + searchId + "is  not Present.");
+                    break;
+                case 4:
+                    System.out.println("Enter the employee Id to search how often:");
+                    searchId = getInput();
+                    int numberOfTimes = employeeTree.howOften(employeeTree.root, searchId);
+                    if (numberOfTimes == 0)
+                        System.out.println("Employee Id " + searchId + "has not entered the org.");
+                    else
+                        System.out.println("Employee Id " + searchId + "has entered " + numberOfTimes + " times");
+                    break;
+                case 5:
+                    EmployeeNode frequentVisitor = employeeTree.frequentVisitor(employeeTree.root);
+                    if (frequentVisitor == null)
+                        System.out.println("Tree is empty");
+                    else
+                        System.out.println("Frequent visitor employee id " + frequentVisitor.empId + " has entered " + frequentVisitor.attCount + " times");
+                    break;
+                case 6:
+                    System.out.println("Not yet implemented:");
+                    break;
+                default:
+                    System.out.println("Invalid Input");
+                    break;
+                }
+            }// End of try
+            catch(NumberFormatException e)
+            {
+                System.out.println("Input is invalid, Enter a valid input.");
+                continue;
+            }
+        } // End of while
+    }
+    
+    static int getInput() throws NumberFormatException
+    {
+        int input = -1;
+        Scanner sc = new Scanner(System.in);
+        
+        input = Integer.parseInt(sc.nextLine());
+
+        return input;
     }
         
 }
